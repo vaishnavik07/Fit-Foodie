e62d47d47a7dcb14a69b1160313f7b429e2d3615
Vaishnavi Katkar <85441257+vaishnavik07@users.noreply.github.com>
2022-10-19 09:38:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.secret_key = 'xyz623'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Vrk17#2002'
+app.config['MYSQL_PASSWORD'] = '**********'
 app.config['MYSQL_DB'] = 'pythonlogin'
 
 
